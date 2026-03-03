5e92639eb57fa1b972d36189e1951677a426f3db
liviialima <livialimasilva19@gmail.com>
2026-03-03 14:42:02 -0300

    Add files via upload

---
+++ UC - algoritmo (03-03 - 26)/Atv1.py
@@ -0,0 +1,8 @@
+numero1 = float(input("Qual o primeiro número?"))
+numero2 = float(input("Qual o primeiro número?"))
+
+soma = numero1 + numero2
+produto = numero1 * numero2
+
+print("A soma é:",soma)
+print("O produto é:",produto)
---
+++ UC - algoritmo (03-03 - 26)/Atv2.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Qual o número?"))
+caso1 = numero1 ** 2
+caso2 = numero2 ** 3
+
+if(numero1 % 2 == 0)
+("A resposta é:", caso1)
+
+else:
+print("A resposta é:", caso2)
