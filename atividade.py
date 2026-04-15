080c4e07332592ec6a27b3ec25e8360f971bfc14
liviialima <livialimasilva19@gmail.com>
2026-04-15 10:16:44 -0300

    Implement calculos function for grade calculation
    Fix syntax errors and improve input handling in calculos function.

---
+++ atividade.py
@@ -0,0 +1,13 @@
+def calculos(a,b,c):
+  notas =[]
+  try:
+    for in ranger(1,4):
+    nota = float(input(f" digite sua nota{i}:")
+    notas,append(nota)
+mrdia = sum(notas) / len(notas)
+ print(f" a nota final é: {media:.2f}"
+
+  exept ValueError:
+    print("digite somente numeros")
+
+    calculos()
